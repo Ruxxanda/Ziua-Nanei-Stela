39d9945f9a5d65c45217056a66f3256b383573e1
Ruxanda <153311945+Ruxxanda@users.noreply.github.com>
2024-11-19 16:08:35 +0200

    Create surpriza.js

---
+++ js/surpriza.js
@@ -0,0 +1,13 @@
+// După ce animația se termină, afișăm secțiunea principală
+document.addEventListener('DOMContentLoaded', () => {
+    const animație = document.getElementById('animație');
+    const principal = document.getElementById('principal');
+  
+    // Setăm timpul după care se termină animația
+    setTimeout(() => {
+      animație.style.display = 'none'; // Ascundem animația
+      principal.style.display = 'block'; // Afișăm secțiunea principală
+      document.body.style.overflow = 'auto'; // Reafisăm scrollbarul
+    }, 6000); // 3 secunde, cât durează animația
+  });
+  
